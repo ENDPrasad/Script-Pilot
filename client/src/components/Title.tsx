50c0634fd6dc91e48cdc839bd1ff499ce98b0670
E.N.D. Prasad <e.n.d.prasad@gmail.com>
2024-11-03 17:14:29 +0530

    change logo to svg

--- client/src/components/Title.tsx
+++ client/src/components/Title.tsx
@@ -6,7 +6,18 @@ function Title(){
     return <div className="title-section">
         <div className="title">
         <div className="logo-section">
-        <img className='logo' src='./Dbugit3.png'/>
+        {/* <img className='logo' src='./Dbugit3.png'/> */}
+        <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 2000 2000" width={150}>
+<path transform="translate(318,660)" d="m0 0h1102l20 2 24 4 30 7 24 7 26 10 23 11 19 11 11 8 13 9 16 13 20 18 19 19 9 11 12 16 10 15 9 15 14 27 11 28 8 27 6 32 3 25v47l-4 32-6 28-6 22-9 24-12 26-11 20-9 14-10 14-11 14-15 16-12 12-8 7-9 8-11 8-17 12-20 12-18 10-25 11-23 8-23 7-25 5-26 4-28 3h-1090l-12-5-7-6-6-9-4-11-1-9v-604l4-14 8-11 10-7z" fill="#1E4A7B"/>
+<path transform="translate(1078,920)" d="m0 0h9l15 2 15 6 11 6 11 11 1-7 5-9 8-6 10-3h30l12 3 6 4 6 8 2 5 1 7v220l-2 18-6 20-8 16-10 14-12 12-12 10-17 10-14 6-17 5-14 2h-26l-19-3-17-5-20-9-14-8-12-10-6-10v-13l4-11 7-11 15-15 9-5h9l10 5 10 7 13 6 15 4h18l11-4 8-6 6-7 5-10 3-14 1-15 1-4-8 7-10 7-15 6-11 2h-26l-16-4-12-5-14-8-13-11-14-14-12-17-9-19-5-18-2-13v-25l3-17 5-16 8-16 7-11 12-14 8-8 17-13 15-8 16-5z" fill="#FDFDFD"/>
+<path transform="translate(439,803)" d="m0 0h19l12 2 9 4 5 4 5 10 2 14 1 104 11-9 14-7 16-4h22l16 3 15 6 14 8 10 8 12 11 9 10 9 13 10 19 5 15 3 17 1 10v10l-2 17-4 17-6 15-9 16-11 14-7 8h-2v2l-11 9-13 9-14 7-16 5-6 1h-25l-14-3-13-5-11-8-5-5-4 8-4 5-12 6-7 2-9 1h-13l-16-3-9-5-6-9-3-9-1-9v-295l1-14 4-11 7-8 9-4z" fill="#FDFDFD"/>
+<path transform="translate(719,920)" d="m0 0h23l11 2 10 5 5 5 3 4 2 6 1 8 1 102 3 12 6 10 8 7 10 4 4 1h19l13-5 8-6 7-11 3-10 1-104 2-11 4-8 7-6 11-4 6-1h24l10 2 9 4 5 5 4 7 2 10v196l-2 9-4 8-7 6-14 4-10 1h-13l-13-2-10-4-6-7-3-9v-8h-2l-2 4-11 10-14 9-14 5-5 1h-21l-17-3-19-7-13-8-10-8-11-11-8-11-8-14-7-16-4-15-3-19v-114l4-11 4-6 9-5z" fill="#FDFDFD"/>
+<path transform="translate(1439,835)" d="m0 0h26l13 3 10 7 5 10 1 7v61l27-2h14l12 2 10 5 5 6 4 10 1 7v24l-3 13-6 10-12 6-6 1h-30l-16-2 1 73 3 10 2 4 7 2 19 1 10 2 10 5 5 7 3 12v24l-3 12-3 6-4 5-10 4-10 2h-24l-18-2-19-5-16-7-13-10-9-11-8-17-4-17-2-18v-82l-11 2h-13l-10-5-6-8-3-9-1-5v-26l3-15 5-8 9-6 5-2h11l11 2v-64l4-11 7-8 11-4z" fill="#FDFDFD"/>
+<path transform="translate(1288,921)" d="m0 0h23l13 3 10 6 5 7 2 7 1 13v181l-2 14-5 10-7 6-10 3-8 1h-22l-14-3-9-6-5-9-2-8v-200l4-11 4-6 6-4 10-3z" fill="#FDFDFD"/>
+<path transform="translate(1091 1e3)" d="m0 0h22l13 4 9 5 9 8 7 10 4 10 2 10v15l-3 12-5 10-7 8-11 8-9 4-12 2h-15l-12-3-9-5-12-11-4-5-5-12-1-3v-26l3-11 5-10 10-11 14-7z" fill="#FD5B5D"/>
+<path transform="translate(537 1e3)" d="m0 0h15l13 4 12 6 8 8 7 11 4 10 1 6v17l-2 10-7 14-12 11-14 7-13 2h-12l-14-3-10-6-13-12-6-12-2-7-1-16 2-16 7-14 10-10 12-6z" fill="#FE5C5D"/>
+<path transform="translate(1290,803)" d="m0 0h19l12 2 9 4 5 5 4 7 2 6 1 8v21l-2 11-5 10-6 5-11 4-7 1h-22l-12-2-10-5-6-9-3-11v-32l3-10 8-9 9-4z" fill="#FDFDFD"/>
+</svg>
         </div>
         <p className='title-description'><i>Debugit</i> allows testers to write and execute code dynamically, enabling them to validate each piece of code as it is added. This tool provides real time feedback, ensuring that each test step works correctly before moving on to the next step which we will be writing on the fly.</p>
         </div>
